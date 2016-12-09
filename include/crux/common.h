c898d17f290b027dc4cf29ddaad0953d8c6fad37
Jeremy Larkin <jeremylarkin@gmail.com>
2016-12-08 21:44:11 -0800

    rename check macros

--- include/crux/common.h
+++ include/crux/common.h
@@ -3,6 +3,8 @@
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
 #define xcontainer(ptr, type, member) __extension__ ({ \
 	const __typeof (((type *)0)->member) *__mptr = (ptr); \
--- include/crux/err.h
+++ include/crux/err.h
@@ -15,7 +15,7 @@ xerr_abort (int code);
 extern void __attribute__ ((format (printf, 2, 3)))
 xerr_fabort (int code, const char *fmt, ...);
 
-#define xerr_check(f) __extension__ ({ \
+#define xcheck(f) __extension__ ({ \
 	int __code = (f); \
 	if (__code < 0) { \
 		xerr_fabort (__code, "%s:%d: '%s' failed", __FILE__, __LINE__, #f); \
@@ -23,7 +23,7 @@ xerr_fabort (int code, const char *fmt, ...);
 	__code; \
 })
 
-#define xerrno_check(f) __extension__ ({ \
+#define xcheck_errno(f) __extension__ ({ \
 	int __code = (f); \
 	if (__code < 0) { \
 		xerr_fabort (errno, "%s:%d: '%s' failed", __FILE__, __LINE__, #f); \
