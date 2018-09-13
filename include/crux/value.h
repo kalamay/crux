bc5c7172bcc7826a67c93a66796f1f06e9fd1772
Jeremy Larkin <jeremylarkin@gmail.com>
2018-09-12 17:23:04 -0700

    add xvalue member for const void *

--- include/crux/value.h
+++ include/crux/value.h
@@ -8,14 +8,16 @@ union xvalue {
 	int64_t i64;
 	double dbl;
 	void *ptr;
+	const void *cptr;
 	int i;
 };
 
-#define XPTR(v) ((union xvalue){ .ptr = (v) })
-#define XU64(v) ((union xvalue){ .u64 = (v) })
-#define XI64(v) ((union xvalue){ .i64 = (v) })
-#define XDBL(v) ((union xvalue){ .dbl = (v) })
-#define XINT(v) ((union xvalue){ .i = (v) })
+#define XPTR(v)  ((union xvalue){ .ptr = (v) })
+#define XCPTR(v) ((union xvalue){ .cptr = (v) })
+#define XU64(v)  ((union xvalue){ .u64 = (v) })
+#define XI64(v)  ((union xvalue){ .i64 = (v) })
+#define XDBL(v)  ((union xvalue){ .dbl = (v) })
+#define XINT(v)  ((union xvalue){ .i = (v) })
 #define XZERO XU64(0)
 #define XNULL XPTR(NULL)
 
